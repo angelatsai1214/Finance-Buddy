db22437fa3b9047def9af9b6122ae7683782ed19
Angela Tsai <64194102+angelatsai1214@users.noreply.github.com>
2022-12-25 13:54:07 -0800

    Add files via upload

---
+++ client/src/API.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const serverURL = 'http://localhost:8000'
+
+const API = {
+    getPurchase: function() {
+        return axios.get(`${serverURL}/api/purchases`);
+    },
+
+    createPurchase: function(payload) {
+        return axios.post(`${serverURL}/api/purchases`, payload);
+    },
+
+    deletePurchase: function(payload) {
+        return axios.delete(`${serverURL}/api/purchases`, { payload });
+    }
+
+}
+export default API;
